209921679d0023751877ad6c234b05b2eb029f40
Josue Vasquez <143981307+ElBosu@users.noreply.github.com>
2025-07-16 20:10:27 -0400

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,6 @@ const routes = [
   { path: '/', name: 'home', component: () => import('../views/HomeView.vue') },
   { path: '/pokedex', name: 'pokedex', component: () => import('../views/PokedexLayout.vue') },
   { path: '/favoritos', name: 'favoritos', component: () => import('../views/FavoriteView.vue') },
-  { path: '/equipo', name: 'Equipo', component: () => import('../views/TeamView.vue')}
 ]
 
 const router = createRouter({
